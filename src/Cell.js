658263de9f93f5a6319dc3a3a095c0a9801f079b
Dmg5th <dmgould310@gmail.com>
2021-02-05 16:07:20 -0500

    commit tree 02a7d5663f9b9f1739943b145dac0fada5ebc33d

---
+++ src/Cell.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component{
+
+    constructor(props){
+        super()
+
+        this.state = {
+            color: props.value
+        }
+        
+    }
+
+    changeColor = () => {
+        this.setState({
+            color: "#333"
+        })
+    }
+
+    render() {
+        return (
+            <div className='cell' 
+            onClick={this.changeColor}
+            style={{backgroundColor: this.state.color}}>
+                
+            </div>
+        )
+    }
+
+  
+}
+
+
+
+
+
